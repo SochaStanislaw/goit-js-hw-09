824f602a999ab44ad357fc4c4b621786ec2b5061
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-04-22 22:57:49 +0200

    update#3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,17 @@
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const saveVideoTime = data => {
+function saveVideoTime(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
-};
+}
 
-const getSavedVideo = () => {
+function getSavedVideo() {
   const savedVideo = localStorage.getItem('videoplayer-current-time');
-};
+  player.setCurrentTime(savedVideo ? parseFloat(savedVideo) : 0);
+}
 
-const resumeSavedVideo = () => {
-  const lastPlayedTime = getSavedVideo();
-  player.setCurrentTime(lastPlayedTime);
-};
-
-resumeSavedVideo();
-
+getSavedVideo();
 player.on('timeupdate', throttle(saveVideoTime, 1000));
